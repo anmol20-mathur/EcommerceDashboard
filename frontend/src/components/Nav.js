7d1299690f97ce04bdecabc6189ed3089debf176
anmol20-mathur <60462765+anmol20-mathur@users.noreply.github.com>
2022-11-27 21:19:29 +0530

    header and footer changes

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div>
-        <ul className='nav-ul'>
-            <li><Link to="/">Products</Link></li>
-            <li><Link to="/addProducts">Add Products</Link></li>
-            <li><Link to="/update">Update</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-            <li><Link to="/profileView">Profile View</Link></li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <ul className="navbar-nav mx-2">
+            <li className='nav-item active mx-2'><Link to="/addProducts" className='nav-link'>Add Products</Link></li>
+            <li className='nav-item mx-2'><Link to="/update"  className='nav-link'>Update</Link></li>
+            <li className='nav-item mx-2'><Link to="/logout"  className='nav-link'>Logout</Link></li>
+            <li className='nav-item mx-2'><Link to="/profileView"  className='nav-link'>Profile View</Link></li>
+            <li className='nav-item mx-2'><Link to="/signup"  className='nav-link'>Sign Up</Link></li>
+        </ul> 
+      </nav>
     </div>
   )
 }
